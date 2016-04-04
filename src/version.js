9a800a9d66cd848868694aab4af7ff000a29ebc1
cenfun <cenfun@gmail.com>
2016-04-04 19:46:01 +0800

    add version

---
+++ src/version.js
@@ -0,0 +1,17 @@
+(function() {
+
+    var version = {
+
+        //每次release新版，修改下面build日期
+        build: "160403",
+
+        //大版本号
+        version: "5.0"
+
+    };
+
+
+    return version;
+
+
+}());
