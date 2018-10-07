f761bbaa0bb84010d46e2bfbdeb514d69c893dc5
cenfun <cenfun@gmail.com>
2018-10-07 23:17:36 +0800

    update demo

--- src/mixer/mixer.js
+++ src/mixer/mixer.js
@@ -29,11 +29,13 @@ class CMPMixer extends ViewBase {
     createAnalyser() {
         var analyser = this.audioContext.createAnalyser();
 
-        //var source = this.audioContext.createMediaElementSource(this.option.audio);
+        console.log(analyser);
 
-        //source.connect(analyser);
+        var source = this.audioContext.createMediaElementSource(this.option.audio);
 
-        //analyser.connect(this.audioContext.destination);
+        source.connect(analyser);
+
+        analyser.connect(this.audioContext.destination);
 
         // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
         // analyser.fftSize = 64;
@@ -43,25 +45,59 @@ class CMPMixer extends ViewBase {
 
         this.canvas = this.find("canvas").get(0);
 
+        this.ctx = this.canvas.getContext('2d');
+
+        this.gradient = this.ctx.createLinearGradient(0, 0, 0, 300);
+        this.gradient.addColorStop(1, '#0f0');
+        this.gradient.addColorStop(0.5, '#ff0');
+        this.gradient.addColorStop(0, '#f00');
+
+        this.drawMixer(analyser);
+    }
+
+    drawMixer(analyser) {
+
         var cwidth = this.canvas.width;
         var cheight = this.canvas.height - 2;
+
         var meterWidth = 10;
-        var gap = 2;
         var capHeight = 2;
         var capStyle = '#fff';
         var meterNum = 800 / (10 + 2);
         var capYPositionArray = [];
 
-        var ctx = this.canvas.getContext('2d');
-        var gradient = ctx.createLinearGradient(0, 0, 0, 300);
-        gradient.addColorStop(1, '#0f0');
-        gradient.addColorStop(0.5, '#ff0');
-        gradient.addColorStop(0, '#f00');
+        var array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
 
-        this.drawMixer();
-    }
+        //console.log(1);
 
-    drawMixer() {
+
+        var step = Math.round(array.length / meterNum);
+        this.ctx.clearRect(0, 0, cwidth, cheight);
+
+        for (var i = 0; i < meterNum; i++) {
+            var value = array[i * step];
+            if (capYPositionArray.length < Math.round(meterNum)) {
+                capYPositionArray.push(value);
+            }
+            this.ctx.fillStyle = capStyle;
+            //draw the cap, with transition effect
+            if (value < capYPositionArray[i]) {
+                this.ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
+            } else {
+                this.ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
+                capYPositionArray[i] = value;
+            }
+            this.ctx.fillStyle = this.gradient;
+
+            this.ctx.fillRect(i * 12, cheight - value + capHeight, meterWidth, cheight);
+        }
+
+        var self = this;
+        requestAnimationFrame(function() {
+            self.drawMixer(analyser);
+        });
+
 
     }
 
